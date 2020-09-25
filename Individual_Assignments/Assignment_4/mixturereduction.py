ead57d57d0ba4d5ea0f3216f9dfdcd925d5adafe
Sander Furre <sander.furre@gmail.com>
2020-09-25 15:07:42 +0200

    Update mixturereductions script, the implementation I had was wrong (will debug later perhaps)

--- Individual_Assignments/Assignment_4/mixturereduction.py
+++ Individual_Assignments/Assignment_4/mixturereduction.py
@@ -4,39 +4,27 @@ import numpy as np
 
 
 def gaussian_mixture_moments(
-    w: np.ndarray,  # the mixture weights shape=(N,)
-    mean: np.ndarray,  # the mixture means shape(N, n)
-    cov: np.ndarray,  # the mixture covariances shape (N, n, n)
-) -> Tuple[
-    np.ndarray, np.ndarray
-]:  # the mean and covariance of of the mixture shapes ((n,), (n, n))
+    w: np.ndarray,  # the mixture weights, shape=(N,)
+    mean: np.ndarray,  # the mixture means, shape(N, n)
+    cov: np.ndarray,  # the mixture covariances, shape (N, n, n)
+    ) -> Tuple[np.ndarray, np.ndarray]:
+         # the mean and covariance of of the mixture, shapes ((n,), (n, n))
     """Calculate the first two moments of a Gaussian mixture"""
-
     # mean
-    mean_bar = np.average(mean,weights=w, axis=0)  # DONE
-    
-    """
-    Comment (to myself, remove it later)
-        - It was a bit unclear if the gaussian means were row or column vectors, but based
-        on the shapes if mean and cov in the input i believe they are column-vectors (shape: (n,))
-    """
+    mean_bar = np.average(mean, weights=w, axis=0)
 
     # covariance
     # # internal covariance
-    cov_int = np.average(cov,axis=0,weights=w)  # DONE
+    cov_int = np.average(cov, weights=w, axis=0)
 
     # # spread of means
-    # Optional calc: mean_diff =
-    mean_diff = mean-mean_bar #Tested this method with a matrix of mockmeans and a mockavg
-    #cov_ext = np.average(mean_diff.T@mean_diff,axis=0,weights=w)  # DID NOT GET THIS WAY TO WORK
-    cov_ext = mean_diff.T @ np.diag(w) @ mean_diff
-    # shapes:    (n, N)   @   (N, N)   @  (N, n)  = (n, n)
-    """
-    Comment to understand the code above: Had to take mean_diff.T@mean_diff instead of mean_diff@mean_diff.T
-    which is in the formula 6.21 because we have row mean vectors and the equation assumes column vectors.
-    """
+    N, n = mean.shape
+    cov_ext = np.zeros((n,n))
+    for i in range(N):
+        mean_diff = (mean[i] - mean_bar).reshape((-1,1))
+        cov_ext += (cov[i] + mean_diff @ mean_diff.T) * w[i]
 
     # # total covariance
-    cov_bar = cov_int+cov_ext  # DONE
+    cov_bar = cov_int + cov_ext
 
     return mean_bar, cov_bar
