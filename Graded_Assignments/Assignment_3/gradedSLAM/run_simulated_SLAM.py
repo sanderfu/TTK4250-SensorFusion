6aa007b21503e2c2db4b9f62cfe0d988ad487861
Sander Furre <sanderfu@stud.ntnu.no>
2020-11-08 20:58:06 +0100

    Good ANEES results in this tuning

--- Graded_Assignments/Assignment_3/gradedSLAM/run_simulated_SLAM.py
+++ Graded_Assignments/Assignment_3/gradedSLAM/run_simulated_SLAM.py
@@ -96,15 +96,15 @@ K = len(z)
 M = len(landmarks)
 
 # %% Initilize
-Q = np.diag([0.1**2,0.5*2,(1*np.pi/180)**2]) #INITDONE
-R = 0.7*np.diag([0.1**2, (1*np.pi/180)**2]) #INITDONE
+Q = np.diag([0.012**2,0.009*2,(0.351*np.pi/180)**2]) #INITDONE
+R = np.diag([0.1**2, (1*np.pi/180)**2]) #INITDONE
 
 
 
 
 doAsso = True
 
-JCBBalphas = np.array([0.005, 0.005])  #INITDONE first is for joint compatibility, second is individual
+JCBBalphas = np.array([0.0001, 0.0001])  #INITDONE first is for joint compatibility, second is individual
 # these can have a large effect on runtime either through the number of landmarks created
 # or by the size of the association search space.
 
@@ -140,7 +140,7 @@ if doAssoPlot:
     figAsso, axAsso = plt.subplots(num=1, clear=True)
 
 # %% Run simulation
-N = 100
+N = 1000
 
 print("starting sim (" + str(N) + " iterations)")
 
@@ -234,9 +234,8 @@ ax3.plot(NISnorm[:N], lw=0.5)
 ax3.set_title(f'NIS, {insideCI.mean()*100}% inside {confprob*100}% CI')
 
 
-avg_measurement_count = total_z_dimension_count/len(z)
 ANIS = np.mean(NISnorm[:N])
-CI_ANIS = np.array(chi2.interval(confprob,avg_measurement_count*N))/N
+CI_ANIS = np.array(chi2.interval(confprob,2*N))/N
 print(f"ANIS: {ANIS}")
 print(f"CI ANIS: {CI_ANIS}")
 
